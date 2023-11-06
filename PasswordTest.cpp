12bec4dcfb2ecb55659dfda499b2db5e1d5d989a
nandiniraut <59910074+nandiniraut@users.noreply.github.com>
2023-11-06 11:38:51 -0800

    Update PasswordTest.cpp

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,26 +1,50 @@
 /**
  * Unit Tests for Password class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public ::testing::Test
 {
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+        PasswordTest() {}                  // constructor runs before each test
+        virtual ~PasswordTest() {} // destructor cleans up after tests
+        virtual void SetUp() {}    // sets up before each test (after constructor)
+        virtual void TearDown() {} // clean up after each test, (before destructor)
 };
 
 TEST(PasswordTest, smoke_test)
 {
-    ASSERT_TRUE( 1 == 1 );
+        ASSERT_TRUE(1 == 1);
 }
 TEST(PasswordTest, single_letter_password)
 {
-    Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1,actual);
+        Password my_password;
+        int actual = my_password.count_leading_characters("Z");
+        ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, letters_repeated_at_end)
+{
+        Password pass;
+        ASSERT_EQ(1, pass.count_leading_characters("Zoo"));
+}
+
+TEST(PasswordTest, letters_repeated_at_beginning)
+{
+        Password pass;
+        ASSERT_EQ(3, pass.count_leading_characters("AAApassword"));
+}
+
+TEST(PasswordTest, no_letters_repeated)
+{
+        Password pass;
+        ASSERT_EQ(1, pass.count_leading_characters("a"));
+}
+
+TEST(PasswordTest, empty_password)
+{
+        Password pass;
+        ASSERT_EQ(1, pass.count_leading_characters(""));
 }
